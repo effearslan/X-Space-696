a75fb6b51fcefac66e837aac3fa3297405f830cf
Tural Naghibayli <tural.naghibayli@gmail.com>
2021-09-14 16:43:59 +0300

    Added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,33 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :update, :destroy]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to @booking, notice: 'booking was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_space
+    @booking = Booking.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def space_params
+    params.require(:space).permit(:status, :total_price)
+  end
 end
