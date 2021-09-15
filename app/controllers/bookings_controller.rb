9515b956d7157809b523cf2b3316d505a030af1f
Tural Naghibayli <tural.naghibayli@gmail.com>
2021-09-15 11:43:42 +0300

    Updated Booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,20 +5,21 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
-  def new
-    @booking = Booking.new
-  end
 
   def index
     @bookings = Booking.all
   end
 
   def create
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new
+    @space = Space.find(params[:space_id])
     @booking.user = current_user
+    @booking.space = @space
+    @booking.total_price = @space.price + 10
+    @booking.status = "Pending"
 
     if @booking.save
-      redirect_to @booking, notice: 'booking was successfully created.'
+      redirect_to spaces_path, notice: 'booking was successfully created.'
     else
       render :new
     end
@@ -26,12 +27,13 @@ class BookingsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-  def set_space
+  def set_booking
     @booking = Booking.find(params[:id])
   end
 
+
   # Only allow a trusted parameter "white list" through.
-  def space_params
-    params.require(:space).permit(:status, :total_price)
-  end
+  # def booking_params
+  #   params.require(:booking).permit(:status, :total_price)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :spaces, only: [:show, :new, :create, :index, :edit, :update] do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:index, :new, :create]
   end
 
   # /spaces/id/bookings/new
